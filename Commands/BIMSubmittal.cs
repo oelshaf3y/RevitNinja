7dbc1a11ea2dac2e3cec6193327fc1a1db3b5472
oelshaf3y <o.elshaf3y@gmail.com>
2025-08-20 13:54:19 +0300

    ultimate bim submittal

--- Commands/BIMSubmittal.cs
+++ Commands/BIMSubmittal.cs
@@ -16,6 +16,8 @@ namespace Revit_Ninja.Commands
         UIDocument uidoc;
         Document doc;
         View3D new3DView, duplicate;
+        UIApplication uiapp;
+
         List<ElementId> excluded = new List<ElementId>();
         StringBuilder sb = new StringBuilder();
         List<ViewSection> sections = new List<ViewSection>();
@@ -25,6 +27,7 @@ namespace Revit_Ninja.Commands
         {
             uidoc = commandData.Application.ActiveUIDocument;
             doc = uidoc.Document;
+            uiapp = commandData.Application;
 
             if (!doc.getAccess()) return Result.Failed;
 
@@ -49,7 +52,21 @@ namespace Revit_Ninja.Commands
                 if (window.DeleteNOSCB.IsChecked == true) try { NOS(); } catch { sb.AppendLine("Failed to delete unused views!"); }
                 if (window.DelCADCB.IsChecked == true) try { delCad(); } catch { sb.AppendLine("Failed to remove CAD drawings!"); }
                 if (window.RemoveLinksCB.IsChecked == true) try { removeLinks(); } catch { sb.AppendLine("Failed to remove links!"); }
-                if (window.SectionsAndSheetsCB.IsChecked == true) try { populateSections(commandData); } catch (Exception ex) { sb.AppendLine("Failed to populate sections!"); doc.print(ex.Message); }
+                if (window.SectionsAndSheetsCB.IsChecked == true) try { populateSections(commandData); } catch (Exception ex) { sb.AppendLine("Failed to populate sections!"); }
+                if (window.purgeFilters.IsChecked == true) try { purgeFilters(); } catch { sb.AppendLine("Failed to purge filters"); }
+                if (window.purgeSets.IsChecked == true) try { purgeSets(); } catch { sb.AppendLine("Failed to purge/create publish sets"); }
+                if (window.purgeBrowser.IsChecked == true) try { purgeBrowser(); } catch { sb.AppendLine("Failed to reset browser organization"); }
+
+
+                if (window.purge.IsChecked == true)
+                {
+                    doc.print("Run Purge Unused for about 5 times to insure that all unuesd elements are removed\nPlease hit enter until number of items checked reaches 0");
+                    try
+                    {
+                        uiapp.PostCommand(RevitCommandId.LookupPostableCommandId(PostableCommand.PurgeUnused));
+                    }
+                    catch { sb.AppendLine("Failed to run revit internal purge"); }
+                }
                 TG.Assimilate();
             }
             if (sb.Length > 0) doc.print(sb.ToString());
@@ -131,7 +148,7 @@ namespace Revit_Ninja.Commands
                     try
                     {
 
-                        if (category != null && category.CategoryType == CategoryType.Annotation)
+                        if (category != null && category.CategoryType == CategoryType.Model)
                         {
                             if (new3DView.GetCategoryHidden(category.Id) == false)
                             {
@@ -667,6 +684,149 @@ namespace Revit_Ninja.Commands
                 tr.Commit();
             }
             doc.print("Removed " + count + " filters.");
+        }
+
+        public void purgeSets()
+        {
+            List<Element> sets = new FilteredElementCollector(doc).OfClass(typeof(ViewSheetSet)).ToList();
+            List<View> allviews = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Views)
+                .WhereElementIsNotElementType()
+                .Cast<View>().Where(x => x.IsTemplate == false && x.ViewType != ViewType.Internal)
+                .Where(x => x.GetType() != typeof(ViewSheet) && x.GetType() != typeof(ViewSchedule)
+                        && x.ViewType != ViewType.ProjectBrowser && x.ViewType != ViewType.SystemBrowser
+                        && x.ViewType != ViewType.DraftingView && x.ViewType != ViewType.Legend)
+                .ToList();
+
+            List<viewforset> view3dForSet = new List<viewforset>();
+            List<viewforset> view2dForSet = new List<viewforset>();
+            List<viewforset> sheetsForSet = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets)
+                .WhereElementIsNotElementType()
+                .Select(x => new viewforset(x.Name, false, x.Id)).ToList();
+
+            foreach (View view in allviews)
+            {
+                if (view is View3D)
+                    view3dForSet.Add(new viewforset(view.Name, false, view.Id));
+                else view2dForSet.Add(new viewforset(view.Name, false, view.Id));
+            }
+
+            publishSet setWindow = new publishSet();
+            setWindow.list3D.ItemsSource = view3dForSet;
+            setWindow.list2D.ItemsSource = view2dForSet;
+            setWindow.listSheets.ItemsSource = sheetsForSet;
+            setWindow.ShowDialog();
+            using (Transaction tr2 = new Transaction(doc, "Create new set"))
+            {
+                tr2.Start();
+                PrintManager pm = doc.PrintManager;
+                pm.PrintRange = PrintRange.Select;
+                ViewSheetSetting sheetSetting = pm.ViewSheetSetting;
+                var set = sheetSetting.CurrentViewSheetSet;
+                set.Views = new ViewSet();
+                // Add selected 3D views
+                foreach (viewforset v in view3dForSet)
+                {
+                    if (v.IsChecked)
+                    {
+                        View3D view3D = doc.GetElement(v.Id) as View3D;
+                        if (view3D != null)
+                        {
+                            set.Views.Insert(view3D);
+                        }
+                    }
+                }
+                // Add selected 2D views
+                foreach (viewforset v in view2dForSet)
+                {
+                    if (v.IsChecked)
+                    {
+                        View view2D = doc.GetElement(v.Id) as View;
+                        if (view2D != null)
+                        {
+                            set.Views.Insert(view2D);
+                        }
+                    }
+                }
+                // Add selected sheets
+                foreach (viewforset v in sheetsForSet)
+                {
+                    if (v.IsChecked)
+                    {
+                        ViewSheet sheet = doc.GetElement(v.Id) as ViewSheet;
+                        if (sheet != null)
+                        {
+                            set.Views.Insert(sheet);
+                        }
+                    }
+                }
+                sheetSetting.SaveAs(setWindow.setName.Text);
+
+                tr2.Commit();
+            }
+
+
+
+            using (Transaction tr = new Transaction(doc, "Modify sets"))
+            {
+                tr.Start();
+
+                foreach (var s in sets)
+                {
+                    try
+                    {
+                        if (s.Name == setWindow.setName.Text)
+                        {
+                            continue;
+                        }
+                        doc.Delete(s.Id);
+                    }
+                    catch (Exception ex)
+                    {
+                        doc.print($"Error deleting set {s.Name}: {ex.Message}");
+                    }
+                }
+
+                tr.Commit();
+            }
+
+        }
+
+        public void purgeBrowser()
+        {
+            var browser = new FilteredElementCollector(doc).OfClass(typeof(BrowserOrganization)).ToList();
+            using (Transaction tr = new Transaction(doc, "Purge view organization"))
+            {
+                tr.Start();
+                foreach (var elem in browser)
+                {
+                    try
+                    {
+
+                        if (elem.Name == "all") continue;
+                        doc.Delete(elem.Id);
+                    }
+                    catch (Exception ex)
+                    {
+                        doc.print("Error: " + ex.Message);
+                    }
+                }
+                tr.Commit();
+            }
+
+        }
+    }
+
+
+    class viewforset
+    {
+        public string Name { get; set; }
+        public bool IsChecked { get; set; }
+        public ElementId Id { get; set; }
+        public viewforset(string name, bool isChecked, ElementId id)
+        {
+            Name = name;
+            IsChecked = isChecked;
+            Id = id;
         }
     }
 
